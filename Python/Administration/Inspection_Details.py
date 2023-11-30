bafedf575fbbac43ce29edfad377687e9b3babe7
Moksh Vora <151133983+MokshVora24@users.noreply.github.com>
2023-11-30 13:08:22 +0530

    Added Payment and Inspection Details

--- Python/Administration/Inspection_Details.py
+++ Python/Administration/Inspection_Details.py
@@ -1,35 +1,94 @@
 from tkinter import *
 import mysql.connector
 
-conn = mysql.connector.connect(user='root', password='pass@123', host='localhost', database='')
+conn = mysql.connector.connect(user='root', password='pass@123', host='localhost', database='insurance_company')
 cursor = conn.cursor()
-
 class Inspection: 
- 
-    def __init__(self,root): 
-       self.f=Frame(root,height=500,width=500) 
-       self.f.pack()
-       self.l1=Label(text='Inspection Details')
-       self.b1=Button(self.f,text="Submit",command=self.display)
-       self.l1.place(x=50,y=30)
-       self.b1.place(x=200,y=150)
+    def __init__(self, root): 
+        self.f = Frame(root, height=500, width=500, bg='#87CEEB')  # Sky Blue background
+        self.f.pack()
+        
+        # Set Times New Roman font
+        font_times_new_roman = ('Times New Roman', 12)
+        
+        # Accident Details
+        self.l_accident = Label(self.f, text='Accident Details', font=('Times New Roman', 16, 'bold'), bg='#87CEEB', fg='black')  # Bold and black text
+        self.l_date = Label(self.f, text='Date:', font=font_times_new_roman, bg='#87CEEB', fg='black')
+        self.e_date = Entry(self.f, font=font_times_new_roman, bg='#B0C4DE', fg='black')  # Light Steel Blue entry background
+        self.l_time = Label(self.f, text='Time:', font=font_times_new_roman, bg='#87CEEB', fg='black')
+        self.e_time = Entry(self.f, font=font_times_new_roman, bg='#B0C4DE', fg='black')
+        self.l_place = Label(self.f, text='Place:', font=font_times_new_roman, bg='#87CEEB', fg='black')
+        self.e_place = Entry(self.f, font=font_times_new_roman, bg='#B0C4DE', fg='black')
+        
+        # Services
+        self.l_services = Label(self.f, text='Services:', font=('Times New Roman', 14, 'underline'), bg='#87CEEB', fg='black')
+        self.l_addon = Label(self.f, text='AddOns', font=font_times_new_roman, bg='#87CEEB', fg='black')
+        self.addon_options = ['Car Wash', 'Polishing', 'Alignment']
+        self.addon_var = StringVar()
+        self.addon_menu = OptionMenu(self.f, self.addon_var, *self.addon_options)
+        self.l_customer_services = Label(self.f, text='Customer Services:', font=font_times_new_roman, bg='#87CEEB', fg='black')
+        self.customer_services_var = BooleanVar()
+        self.cb_customer_services = Checkbutton(self.f, text=' ', variable=self.customer_services_var, font=font_times_new_roman, bg='#87CEEB', fg='black')
+        
+        # Inspection Report
+        self.l_inspection_report = Label(self.f, text='Accident Report', font=('Times New Roman', 16, 'bold'), bg='#87CEEB', fg='black')
+        self.t_inspection_report = Text(self.f, height=5, width=30, font=font_times_new_roman, bg='#B0C4DE', fg='black')  # Light Steel Blue text area background
+        
+        # Submit Button
+        self.b_submit = Button(self.f, text="Submit", command=self.display, font=font_times_new_roman, bg='#4682B4', fg='white')  # Steel Blue button
+        
+        # Placing widgets on the frame
+        self.l_accident.place(x=50, y=30)
+        self.l_date.place(x=50, y=60)
+        self.e_date.place(x=150, y=60)
+        self.l_time.place(x=50, y=90)
+        self.e_time.place(x=150, y=90)
+        self.l_place.place(x=50, y=120)
+        self.e_place.place(x=150, y=120)
+        
+        self.l_services.place(x=50, y=160)
+        self.l_addon.place(x=50, y=190)
+        self.addon_menu.place(x=100, y=185)
+        self.l_customer_services.place(x=50, y=220)
+        self.cb_customer_services.place(x=160, y=220)
+        
+        self.l_inspection_report.place(x=50, y=300)
+        self.t_inspection_report.place(x=50, y=330)
+        
+        self.b_submit.place(x=50, y=260)
        
     def display(self):
+        accident_date = self.e_date.get()
+        accident_time = self.e_time.get()
+        accident_place = self.e_place.get()
+        
+        addon_services = self.addon_var.get()
+        customer_services = self.customer_services_var.get()
+        
         try: 
-            cursor.execute() 
+            # Assuming you have a table named 'inspection_report'
+            cursor.execute("INSERT INTO inspection_report (accident_date, accident_time, accident_place, addon_services, customer_services) VALUES (%s, %s, %s, %s, %s)", (accident_date, accident_time, accident_place, addon_services, customer_services))
             conn.commit() 
-        except: 
+            print("Data Inserted") 
+        except Exception as e: 
+            print(f"Error: {e}")
             conn.rollback() 
-        print("Data Inserted") 
+        
         conn.close() 
-        self.Insurance_Details() 
+        self.inspection_report()
  
-    def Insurance_Details(self): 
-       root.destroy() 
-       from Administration import Insurance_Details 
- 
- 
-root=Tk() 
+    def inspection_report(self): 
+        try:
+            cursor.execute("SELECT * FROM inspection_report ORDER BY id DESC LIMIT 1")
+            result = cursor.fetchone()
+            if result:
+                report_text = f"Accident Date: {result[1]}\nAccident Time: {result[2]}\nAccident Place: {result[3]}\nAddOn Services: {result[4]}\nCustomer Services: {result[5]}"
+                self.t_inspection_report.delete(1.0, END)
+                self.t_inspection_report.insert(INSERT, report_text)
+        except Exception as e:
+            print(f"Error fetching inspection report: {e}")
+
+root = Tk() 
 root.title("Step 2: Inspection and Accident") 
-obj=Inspection(root) 
+obj = Inspection(root) 
 root.mainloop()
--- Python/Administration/Payment_Details.py
+++ Python/Administration/Payment_Details.py
@@ -1,35 +1,97 @@
 from tkinter import *
 import mysql.connector
 
-conn = mysql.connector.connect(user='root', password='pass@123', host='localhost', database='')
+conn = mysql.connector.connect(user='root', password='pass@123', host='localhost', database='insurance_company')
 cursor = conn.cursor()
 
 class Payment: 
  
-    def __init__(self,root): 
-       self.f=Frame(root,height=500,width=500) 
-       self.f.pack()
-       self.l1=Label(text='Payment Details')
-       self.b1=Button(self.f,text="Submit",command=self.display)
-       self.l1.place(x=50,y=30)
-       self.b1.place(x=200,y=150)
-       
+    def __init__(self, root): 
+        self.root = root
+        self.root.title("Step 4: Payment Options")
+        self.root.geometry("500x500")
+        self.center_window()  # Center the window on the screen
+        self.root.configure(bg="#3498db")  # Blue background color
+        
+        self.f = Frame(root, height=500, width=500, bg="#3498db")  # Blue background color
+        self.f.pack()
+
+        self.l1 = Label(self.f, text='Payment Details', font=("Times New Roman", 20, "bold"), bg="#3498db", fg="black")  # Times New Roman font, larger size
+        self.b1 = Button(self.f, text="Submit", command=self.display, bg="#2ecc71", fg="white")  # Green button
+        self.l1.place(relx=0.5, rely=0.1, anchor="center")  # Center the label
+        self.b1.place(relx=0.5, rely=0.6, anchor="center")  # Center the button
+
+        # Radio buttons for online and offline payment
+        self.payment_var = IntVar()
+        self.online_radio = Radiobutton(self.f, text="Online", variable=self.payment_var, value=1, command=self.show_online_options, bg="#3498db", fg="black", font=("Times New Roman", 14))
+        self.offline_radio = Radiobutton(self.f, text="Offline", variable=self.payment_var, value=2, command=self.show_offline_options, bg="#3498db", fg="black", font=("Times New Roman", 14))
+        self.online_radio.place(relx=0.5, rely=0.35, anchor="center")  # Center the radio button
+        self.offline_radio.place(relx=0.5, rely=0.45, anchor="center")  # Center the radio button
+
+        # Dropdown menu for payment options
+        self.payment_options = StringVar()
+        self.payment_options.set("Select Payment Option")
+        self.payment_menu = OptionMenu(self.f, self.payment_options, "")
+        self.payment_menu.place(relx=0.5, rely=0.5, anchor="center")  # Center the dropdown below radio buttons
+        self.payment_menu.config(state=DISABLED)
+
+    def center_window(self):
+        # Center the window on the screen
+        window_width = self.root.winfo_reqwidth()
+        window_height = self.root.winfo_reqheight()
+        position_right = int(self.root.winfo_screenwidth() / 2 - window_width / 2)
+        position_down = int(self.root.winfo_screenheight() / 2 - window_height / 2)
+        self.root.geometry("+{}+{}".format(position_right, position_down))
+
+    def show_online_options(self):
+        self.payment_menu.config(state=NORMAL)
+        options = ["Card", "Netbanking", "UPI"]
+        self.payment_options.set(options[0])  # Default selection
+        self.payment_menu['menu'].delete(0, 'end')  # Clear previous options
+        for option in options:
+            self.payment_menu['menu'].add_command(label=option, command=lambda value=option: self.payment_options.set(value))
+
+    def show_offline_options(self):
+        self.payment_menu.config(state=NORMAL)
+        options = ["Cheque", "Cash"]
+        self.payment_options.set(options[0])  # Default selection
+        self.payment_menu['menu'].delete(0, 'end')  # Clear previous options
+        for option in options:
+            self.payment_menu['menu'].add_command(label=option, command=lambda value=option: self.payment_options.set(value))
+
     def display(self):
-        try: 
-            cursor.execute() 
-            conn.commit() 
-        except: 
-            conn.rollback() 
-        print("Data Inserted") 
-        conn.close()  
-        self.Display_Details() 
- 
+        payment_type = "Online" if self.payment_var.get() == 1 else "Offline"
+        selected_option = self.payment_options.get()
+
+        # Add your database insertion logic here using payment_type and selected_option
+        try:
+            # Your MySQL connection details
+            conn = mysql.connector.connect(user='root', password='pass@123', host='localhost', database='insurance_company')
+            cursor = conn.cursor()
+
+            # Your MySQL insertion query
+            query = "INSERT INTO payment (payment_type, payment_option) VALUES (%s, %s)"
+            data = (payment_type, selected_option)
+            cursor.execute(query, data)
+
+            conn.commit()
+            conn.close()
+
+            print("Data Inserted")
+            self.Display_Details()
+
+        except Exception as e:
+            print("Error:", e)
+            conn.rollback()
+        print("Data Inserted")
+        conn.close()
+        self.Display_Details()
+
     def Display_Details(self): 
-       root.destroy() 
-       from Administration import Display_Details 
- 
- 
-root=Tk() 
-root.title("Step 4: Payment Options") 
-obj=Payment(root) 
+        self.root.destroy() 
+        # Add your code to navigate to the next page
+
+
+root = Tk() 
+obj = Payment(root) 
 root.mainloop()
